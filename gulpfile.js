9c1474f8bd7c4ffbf4278afe6d430bc7ebb7dad4
Dan Zaharia <zaharia.danny@gmail.com>
2021-05-17 19:23:24 -0400

    final cleanup before starting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	// scsslint = require('gulp-scss-lint') ,
 	browserSync = require('browser-sync'),
 	plumber = require('gulp-plumber'),
 	reload = browserSync.reload;
